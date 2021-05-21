afdd063e4584864d9a319cea76afc16678f3622c
Yshin <m_yangjiabao@163.com>
2021-05-21 11:19:04 +0800

    地图图标对象编码实现

---
+++ mapobjectitem.cpp
@@ -0,0 +1,95 @@
+﻿#include "mapobjectitem.h"
+#include "graphicsmap.h"
+#include <QGraphicsColorizeEffect>
+
+/* XPM */
+static const char *default_xpm[] = {
+/* columns rows colors chars-per-pixel */
+"32 32 9 1 ",
+"  c None",
+". c #19F929",
+"X c #1AF928",
+"o c #1AF929",
+"O c #19FA29",
+"+ c #1AFA29",
+"@ c #1BFA29",
+"# c #1AFB29",
+"$ c #1AFB2A",
+/* pixels */
+"                                ",
+"                                ",
+"               ++               ",
+"              ++++              ",
+"              ++++              ",
+"              ++++              ",
+"           ++++++++++           ",
+"          ++++++++++++          ",
+"        ++++++++++++++++        ",
+"        +++++ ++++ +++++        ",
+"       #++++  ++++  ++++#       ",
+"      +++++    ++    +++++      ",
+"      ++++            +++#      ",
+"      +++              +++      ",
+"   ++++++++          ++++++++   ",
+"  ++++++++++        ++++++++++  ",
+"  ++++++++++        ++++++++++  ",
+"   +++++++++        +++++++++   ",
+"      +++              +++      ",
+"      ++++            ++++      ",
+"      +++++   ++++   +++++      ",
+"       +++++  ++++  +++++       ",
+"        ++++O ++++ +++++        ",
+"        ++++++++++++++++        ",
+"          #+++++++++++          ",
+"           +#++++++#+           ",
+"              ++++              ",
+"              ++++              ",
+"              ++++              ",
+"               ++               ",
+"                                ",
+"                                "
+};
+
+MapObjectItem::MapObjectItem()
+{
+    this->setFlag(QGraphicsItem::ItemIgnoresTransformations, true);
+    setIcon(QString());
+}
+
+void MapObjectItem::setCoordinate(const QGeoCoordinate &coord)
+{
+    if(m_coord == coord)
+        return;
+
+    this->setPos(GraphicsMap::fromCoordinate(coord));
+    emit coordinateChanged(coord);
+}
+
+void MapObjectItem::setIcon(const QString &url)
+{
+    this->setPixmap(url);
+    // Reset to default icon
+    if(pixmap().isNull()) {
+        this->setPixmap(QPixmap(default_xpm));
+    }
+    // make sure that the center of icon is positioned at current position
+    auto boundRect = this->boundingRect();
+    setOffset(-boundRect.center());
+}
+
+void MapObjectItem::setColor(const QColor &color, qreal strength)
+{
+    // We should to unset previous color
+    if(!color.isValid()) {
+        this->setGraphicsEffect(nullptr);
+        return;
+    }
+
+    auto effect = dynamic_cast<QGraphicsColorizeEffect*>(graphicsEffect());
+    if(!effect) {
+        effect = new QGraphicsColorizeEffect;
+        this->setGraphicsEffect(effect);
+    }
+    effect->setColor(color);
+    effect->setStrength(strength);
+}
---
+++ mapobjectitem.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAPOBJECTITEM_H
+#define MAPOBJECTITEM_H
+
+#include "GraphicsMapLib_global.h"
+#include <QGraphicsPixmapItem>
+#include <QGeoCoordinate>
+
+/*!
+ * \brief 地图对象
+ * \details 可用于将图标添加到地图上
+ * \note 该对象不受地图缩放影响，将会始终保持屏幕大小
+ */
+class GRAPHICSMAPLIB_EXPORT MapObjectItem : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+public:
+    MapObjectItem();
+    /// 设置经纬度位置
+    void setCoordinate(const QGeoCoordinate &coord);
+    /// 获取当前经纬度位置
+    const QGeoCoordinate &coordinate() const;
+    /// 设置图标
+    void setIcon(const QString &url);
+    /// 设置图标为纯色，传QColor()可以取消纯色
+    void setColor(const QColor &color, qreal strength = 1.0);
+
+signals:
+    void coordinateChanged(const QGeoCoordinate &coord);
+
+
+private:
+    QGeoCoordinate m_coord;
+};
+
+#endif // MAPOBJECTITEM_H
