a3ef30f6980881147f5ec0b3c4a0d7ecfc0e387e
li.zhao <netzhaoli@163.com>
2021-07-29 14:36:48 +0800

    缺失文件

---
+++ maplineitem.cpp
@@ -0,0 +1,195 @@
+﻿#include "maplineitem.h"
+#include "graphicsmap.h"
+#include <QDebug>
+
+QSet<MapLineItem*> MapLineItem::m_items;
+
+MapLineItem::MapLineItem()
+{
+    //
+    m_items.insert(this);
+
+    //
+    auto pen = this->pen();
+	pen.setWidth(2);
+	pen.setCosmetic(true);  // it will be always 2 pixmap whatever scale transform
+	pen.setCapStyle(Qt::RoundCap);
+	pen.setJoinStyle(Qt::RoundJoin);
+	pen.setColor(Qt::white);
+	this->setPen(pen);
+    //
+	auto font = m_text.font();
+	font.setPointSizeF(14);
+	font.setFamily("Microsoft YaHei");
+	m_text.setFont(font);
+	m_text.setBrush(Qt::yellow);
+	m_text.setPen(QColor(0, 255, 0));
+	m_text.setParentItem(this);
+	m_text.setFlag(ItemIgnoresTransformations);
+    //
+    m_startIcon.setParentItem(this);
+    m_startIcon.setFlag(ItemIgnoresTransformations);
+    m_endIcon.setParentItem(this);
+    m_endIcon.setFlag(ItemIgnoresTransformations);
+}
+
+MapLineItem::~MapLineItem()
+{
+	m_items.remove(this);
+}
+
+void MapLineItem::setStartPoint(const QGeoCoordinate & pt)
+{
+    if(m_endings.first == pt)
+        return;
+    m_endings.first = pt;
+
+    if(!m_endings.second.isValid())
+        m_endings.second = pt;
+
+    updateEndings();
+}
+
+void MapLineItem::setEndPoint(const QGeoCoordinate & pt)
+{
+    if(m_endings.second == pt)
+        return;
+    m_endings.second = pt;
+
+    if(!m_endings.first.isValid())
+        m_endings.first = pt;
+
+    updateEndings();
+}
+
+
+void MapLineItem::setText(const QString & text, Qt::Alignment align)
+{
+	m_text.setText(text);
+	auto lineBound = this->boundingRect();
+	auto textBound = m_text.boundingRect();
+	QPointF pos(-textBound.center());
+	if (align & Qt::AlignLeft) {
+		pos.setX(-lineBound.width()/2);
+	}
+	else if (align & Qt::AlignHCenter) {
+		pos.setX(0);
+	}
+	else if (align & Qt::AlignRight) {
+		pos.setX(lineBound.width() / 2);
+	}
+	if (align & Qt::AlignTop) {
+		pos.setY(- lineBound.height() / 2);
+	}
+	else if (align & Qt::AlignVCenter) {
+		pos.setY(0);
+	}
+	else if (align & Qt::AlignBottom) {
+		pos.setY(lineBound.height() / 2);
+	}
+	m_text.setPos(pos);
+}
+
+void MapLineItem::setTextColor(const QColor & color)
+{
+	m_text.setPen(color);
+}
+
+void MapLineItem::setFontSizeF(const qreal size)
+{
+	auto font = m_text.font();
+	font.setPointSizeF(size);
+	m_text.setFont(font);
+}
+
+void MapLineItem::setLineWidth(const int width)
+{
+	auto pen = this->pen();
+	pen.setWidth(width);
+	this->setPen(pen);
+}
+
+void MapLineItem::setStartIcon(const QPixmap &pixmap, Qt::Alignment align)
+{
+    m_startIcon.setPixmap(pixmap);
+	auto boundRect = m_startIcon.boundingRect();
+	QPointF offset(-boundRect.width() / 2, -boundRect.height());
+
+	if (align & Qt::AlignLeft) {
+		offset.setX(-boundRect.width());
+	}
+	else if (align & Qt::AlignHCenter) {
+		offset.setX(-boundRect.width() / 2);
+	}
+	else if (align & Qt::AlignRight) {
+		offset.setX(0);
+	}
+	if (align & Qt::AlignTop) {
+		offset.setY(-boundRect.height());
+	}
+	else if (align & Qt::AlignVCenter) {
+		offset.setY(0);
+	}
+	else if (align & Qt::AlignBottom) {
+		offset.setY(boundRect.height());
+	}
+	m_startIcon.setOffset(offset);
+    updateEndings();
+}
+
+void MapLineItem::setEndIcon(const QPixmap &pixmap, Qt::Alignment align)
+{
+    m_endIcon.setPixmap(pixmap);
+	auto boundRect = m_endIcon.boundingRect();
+	QPointF offset(-boundRect.width() / 2, -boundRect.height());
+
+	if (align & Qt::AlignLeft) {
+		offset.setX(-boundRect.width());
+	}
+	else if (align & Qt::AlignHCenter) {
+		offset.setX(-boundRect.width() / 2);
+	}
+	else if (align & Qt::AlignRight) {
+		offset.setX(boundRect.width() / 2);
+	}
+	if (align & Qt::AlignTop) {
+		offset.setY(-boundRect.height());
+	}
+	else if (align & Qt::AlignVCenter) {
+		offset.setY(0);
+	}
+	else if (align & Qt::AlignBottom) {
+		offset.setY(boundRect.height());
+	}
+	m_endIcon.setOffset(offset);
+    updateEndings();
+}
+
+const QPair<QGeoCoordinate, QGeoCoordinate> &MapLineItem::endings()
+{
+    return m_endings;
+}
+
+void MapLineItem::attach(MapObjectItem *obj, MapLabelItem *label)
+{
+
+}
+
+const QSet<MapLineItem *> &MapLineItem::items()
+{
+    return m_items;
+}
+
+void MapLineItem::updateEndings()
+{
+    auto ending0 = GraphicsMap::toScene(m_endings.first);
+    auto ending1 = GraphicsMap::toScene(m_endings.second);
+    auto origin = (ending0 + ending1) / 2;
+    auto p0 = ending0 - origin;
+    auto p1 = ending1 - origin;
+    this->setPos(origin);
+    this->setLine({p0, p1});
+    //
+    m_startIcon.setPos(p0);
+    m_endIcon.setPos(p1);
+}
---
+++ maplineitem.h
@@ -0,0 +1,63 @@
+﻿#ifndef MAPLINEITEM_H
+#define MAPLINEITEM_H
+
+#include "GraphicsMapLib_global.h"
+#include <QGraphicsPathItem>
+#include <QGeoCoordinate>
+#include <QTimer>
+#include <QGraphicsPixmapItem>
+
+class MapObjectItem;
+class MapLabelItem;
+
+/*!
+ * \brief 直线
+ */
+class GRAPHICSMAPLIB_EXPORT MapLineItem : public QObject, public QGraphicsLineItem
+{
+    Q_OBJECT
+public:
+	MapLineItem();
+    ~MapLineItem();
+    /// 设置起始点
+	void setStartPoint(const QGeoCoordinate & pt);
+	/// 设置末端点
+	void setEndPoint(const QGeoCoordinate & pt);
+	/// 设置文字
+	void setText(const QString &text, Qt::Alignment align = Qt::AlignCenter);
+	/// 设置文字颜色
+	void setTextColor(const QColor &color);
+	/// 设置文字大小
+	void setFontSizeF(const qreal size);
+	/// 设置线宽
+	void setLineWidth(const int width);
+	/// 设置线段起始图标
+    void setStartIcon(const QPixmap &pixmap, Qt::Alignment align = Qt::AlignCenter);
+	/// 设置线段末端图标
+    void setEndIcon(const QPixmap &pixmap, Qt::Alignment align = Qt::AlignCenter);
+	/// 获取线段两点位置
+    const QPair<QGeoCoordinate, QGeoCoordinate> &endings();
+    /// 依附到地图对象和标签对象，将会自动更新位置
+    void attach(MapObjectItem *obj, MapLabelItem *label);
+    /// 取消依附地图对象，后续手动更新位置
+    void detach();
+
+public:
+    /// 获取所有的实例
+    static const QSet<MapLineItem*> &items();
+
+private:
+    void updateEndings();
+
+private:
+    static QSet<MapLineItem*> m_items;         ///< 所有实例
+
+private:
+    //
+    QGraphicsSimpleTextItem  m_text;
+    QGraphicsPixmapItem      m_startIcon;  ///<起始图标
+    QGraphicsPixmapItem      m_endIcon;    ///<末端图标
+    QPair<QGeoCoordinate, QGeoCoordinate> m_endings;   ///<线段两点
+};
+
+#endif // MAPLINEITEM_H
