3e06661fe0584a6305118321558311f6645ac58a
li.zhao <netzhaoli@163.com>
2021-07-29 16:28:19 +0800

    增加三角形-梯形的组合扇形

--- mappieitem.cpp
+++ mappieitem.cpp
@@ -2,6 +2,7 @@
 #include "graphicsmap.h"
 
 QSet<MapPieItem*> MapPieItem::m_items;
+QSet<MapTriTrapItem*> MapTriTrapItem::m_items;
 
 MapPieItem::MapPieItem() :
     m_coord({0, 0, 0}),
@@ -77,4 +78,120 @@ void MapPieItem::updatePie()
     QPointF bottomRight(rightPoint.rx(), 2*centerPoint.ry() - upPoint.ry());
     QRectF rect(topLeft, bottomRight);
     this->setRect(rect);
+}
+
+MapTriTrapItem::MapTriTrapItem():
+    m_coord({0, 0, 0}),
+    m_near(10e3),
+    m_far(20e3),
+    m_azimuth(0),
+    m_span(0)
+{
+    // keep the outline width of 1-pixel when item scales
+    auto pen = this->pen();
+    pen.setColor(Qt::NoPen);
+    pen.setWidth(1);
+    pen.setCosmetic(true);
+    this->setPen(pen);
+    //
+    m_triangle.setParentItem(this);
+    m_trapezoid.setParentItem(this);
+    pen.setColor(Qt::lightGray);
+    pen.setStyle(Qt::DotLine);
+    m_triangle.setPen(pen);
+    pen.setColor(Qt::red);
+    pen.setStyle(Qt::SolidLine);
+    m_trapezoid.setPen(pen);
+    setAngle(60);
+    //
+    m_items.insert(this);
+}
+
+MapTriTrapItem::~MapTriTrapItem()
+{
+    m_items.remove(this);
+}
+
+void MapTriTrapItem::setCoordinate(const QGeoCoordinate &coord)
+{
+    if(m_coord == coord)
+        return;
+    m_coord = coord;
+    // such will change Rect
+    updateTrapezoid();
+}
+
+void MapTriTrapItem::setNear(const qreal &meter)
+{
+    if(m_near == meter)
+        return;
+    m_near = meter;
+    //
+    updateTrapezoid();
+}
+
+void MapTriTrapItem::setAzimuth(const qreal &degree)
+{
+    if(m_azimuth == m_azimuth)
+        return;
+    m_azimuth = degree;
+    //
+    updateTrapezoid();
+}
+
+void MapTriTrapItem::setAngle(const qreal &degree)
+{
+    if(m_span == degree)
+        return;
+    m_span = degree;
+    //
+    updateTrapezoid();
+}
+
+QGraphicsPolygonItem *MapTriTrapItem::getTriangle()
+{
+    return &m_triangle;
+}
+
+QGraphicsPolygonItem *MapTriTrapItem::getTrapezoid()
+{
+    return &m_trapezoid;
+}
+
+void MapTriTrapItem::updateTrapezoid()
+{
+    auto span_2 = m_span / 2;
+    auto beginAz = m_azimuth - span_2;
+    auto endAz = m_azimuth + span_2;
+    auto coord1 = m_coord.atDistanceAndAzimuth(m_near, beginAz);
+    auto coord2 = m_coord.atDistanceAndAzimuth(m_near, endAz);
+    auto coord3 = m_coord.atDistanceAndAzimuth(m_far, endAz);
+    auto coord4 = m_coord.atDistanceAndAzimuth(m_far, beginAz);
+    auto point0 = GraphicsMap::toScene(m_coord);
+    auto point1 = GraphicsMap::toScene(coord1);
+    auto point2 = GraphicsMap::toScene(coord2);
+    auto point3 = GraphicsMap::toScene(coord3);
+    auto point4 = GraphicsMap::toScene(coord4);
+    {   // Self
+        QPolygonF polygon;
+        polygon.append(point0);
+        polygon.append(point3);
+        polygon.append(point4);
+        this->setPolygon(polygon);
+    }
+    {   // Triangle
+        QPolygonF polygon;
+        polygon.append(point0);
+        polygon.append(point1);
+        polygon.append(point2);
+        m_triangle.setPolygon(polygon);
+    }
+    {   // Trapozoid
+        QPolygonF polygon;
+        polygon.append(point1);
+        polygon.append(point2);
+        polygon.append(point3);
+        polygon.append(point4);
+        m_trapezoid.setPolygon(polygon);
+    }
 }
--- mappieitem.h
+++ mappieitem.h
@@ -36,9 +36,54 @@ private:
 
 private:
     QGeoCoordinate m_coord;     ///< 位置
-    qreal          m_radius;    ///< 长度(半径)
+    qreal          m_radius;    ///< 长度 米
     qreal          m_azimuth;   ///< 朝向，以正北右偏为正
     qreal          m_span;      ///< 张角 度
 };
 
+/*!
+ * \brief 三角形梯形混合类扇形
+ * \details 可用于SAR、红外等效果的威力区
+ * \note 更改三角形和梯形外观，请通过getTriangle和getTrapezoid获取实例，然后更改画笔和画刷
+ */
+class GRAPHICSMAPLIB_EXPORT MapTriTrapItem : public QGraphicsPolygonItem
+{
+public:
+    MapTriTrapItem();
+    ~MapTriTrapItem();
+    /// 设置位置
+    void setCoordinate(const QGeoCoordinate &coord);
+    /// 设置近距长度
+    void setNear(const qreal &meter);
+    /// 设置远距长度
+    void setFar(const qreal &meter);
+    /// 设置朝向
+    void setAzimuth(const qreal &degree);
+    /// 设置张角
+    void setAngle(const qreal &degree);
+    /// 获取三角形
+    QGraphicsPolygonItem *getTriangle();
+    /// 获取梯形
+    QGraphicsPolygonItem *getTrapezoid();
+
+public:
+    /// 获取所有的实例
+    static const QSet<MapTriTrapItem*> &items();
+
+private:
+    void updateTrapezoid();
+
+private:
+    static QSet<MapTriTrapItem*> m_items;         ///< 所有实例
+
+private:
+    QGeoCoordinate m_coord;     ///< 位置
+    qreal          m_near;      ///< 近距 米
+    qreal          m_far;       ///< 远距 米
+    qreal          m_azimuth;   ///< 朝向，以正北右偏为正
+    qreal          m_span;      ///< 张角 度
+    //
+    QGraphicsPolygonItem m_triangle;
+    QGraphicsPolygonItem m_trapezoid;
+};
 #endif // MAPPIEITEM_H
