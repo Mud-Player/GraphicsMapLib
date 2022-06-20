ae3714e531c118f47b8b2c9d1334b22930f42a4e
LiangYu12345 <920424554@qq.com>
2022-06-20 17:10:11 +0800

    更新5所撰写代码

---
+++ mapobjecttooltipitem.cpp
@@ -0,0 +1,106 @@
+﻿#include "mapobjecttooltipitem.h"
+
+mapObjectToolTipItem::mapObjectToolTipItem(const QGeoCoordinate &coord)
+{
+
+}
+
+mapObjectToolTipItem::~mapObjectToolTipItem()
+{
+
+}
+
+void mapObjectToolTipItem::setAnchorPosition(AnchorPosition anchorPos)
+{
+
+}
+
+void mapObjectToolTipItem::setMargins(int left, int top, int right, int bottom)
+{
+
+}
+
+void mapObjectToolTipItem::setSpacing(int space)
+{
+
+}
+
+void mapObjectToolTipItem::addField(const QString &field, bool bVolatile)
+{
+
+}
+
+void mapObjectToolTipItem::inrField(int pos, const QString &field, bool bVolatile)
+{
+
+}
+
+void mapObjectToolTipItem::delField(const QString &field)
+{
+
+}
+
+void mapObjectToolTipItem::setFieldFont(const QString &field, const QFont &font)
+{
+
+}
+
+void mapObjectToolTipItem::setValueFont(const QString &field, const QFont &font)
+{
+
+}
+
+void mapObjectToolTipItem::setFieldPen(const QString &field, const QPen &pen)
+{
+
+}
+
+void mapObjectToolTipItem::setValuePen(const QString &field, const QPen &pen)
+{
+
+}
+
+void mapObjectToolTipItem::setValue(const QString &field, const QString &value)
+{
+
+}
+
+void mapObjectToolTipItem::setBackWidth(int width)
+{
+
+}
+
+void mapObjectToolTipItem::setFixDirect(bool bFixDirect, double fixedangle)
+{
+
+}
+
+void mapObjectToolTipItem::SetRoundedRadius(int roundRadius)
+{
+
+}
+
+void mapObjectToolTipItem::SetBorderPen(const QPen &borderPen)
+{
+
+}
+
+void mapObjectToolTipItem::SetBackBrush(const QBrush &brush)
+{
+
+}
+
+void mapObjectToolTipItem::SetSymbolSize(const QSize &size)
+{
+
+}
+
+QSize mapObjectToolTipItem::TabletSize()
+{
+    return QSize();
+}
+
+QString mapObjectToolTipItem::GetValue(const QString &field)
+{
+    return QString();
+}
